e362de80167427c32d0db2115000fc2aea64b5cf
KyzaaDev <dzakyraufnurfhadillah@gmail.com>
2025-06-07 06:58:18 +0700

    update function name and variable name

---
+++ main.py
@@ -0,0 +1,116 @@
+import requests
+import os
+import colorama 
+from colorama import Fore, Style, Back, init
+import time
+import argparse
+
+colorama.init(autoreset=True)
+
+## digunakan untuk menampilkan banner
+def banner():
+    print(Fore.MAGENTA + r"""
+  _____ _          ___                                
+ |_   _| |_  ___  / __|_ __  __ _ _ __  _ __  ___ _ _ 
+   | | | ' \/ -_) \__ \ '_ \/ _` | '  \| '  \/ -_) '_|
+   |_| |_||_\___| |___/ .__/\__,_|_|_|_|_|_|_\___|_|  
+                      |_|                             
+        """ + Fore.WHITE + "Made by: KyzaaDev\n")
+
+
+## membuat parser untuk user
+parser = argparse.ArgumentParser()
+parser.add_argument("--url", type=str, required=True, help="Enter your URL here")
+parser.add_argument("--loop", type=int, required=False, default=1, help="Enter the number of loops (default: 250)")
+parser.add_argument("--delay", type=int, required=False,default=2, help="Enter the delay for each message (default: 2 detik)")
+args = parser.parse_args()
+
+def validate_api():
+    ## meminta dan melakukan validasi API yg dimasukkan user
+    global urls
+    urls = args.url
+
+    if not urls.startswith("https://api.telegram.org/"):
+        print(Fore.RED + "\n[!] Make sure your API is from telegram!")
+        time.sleep(2)
+        os.system('clear' if os.name == 'posix' else 'cls')
+        exit()
+
+
+## variable yg digunakna untuk menampung pesan dan jumlah req yg akan dikirim
+messages = []   
+
+## Fungsi yang digunakan untuk meminta pesan yang akan dikirim ke API
+def get_user_messages():
+    while True:
+        add_messages = input(Fore.BLUE + "Input your messages here (quit for stop): " + Style.RESET_ALL)
+        if add_messages == "quit":
+            print(Fore.YELLOW + "\n[~] START SENDING MESSAGES!\n")
+            break
+        messages.append(add_messages)
+
+## function untuk membuat variable yang memuat banyak looping
+def loop_amount():
+    global message_amount
+    message_amount = args.loop
+
+## function untuk memuat berapa delay setiap pesan
+def delay():
+    global dely
+    dely = args.delay
+
+
+def validate_input():
+    errors = []
+    if args.loop <= 0:
+        errors.append("[!] The number of loops must be greater than 0.")
+    if args.delay <= 0:
+        errors.append("[!] Delay must be more than 0 seconds.")
+
+
+    if errors:
+        for err in errors:
+            print(Fore.RED + err)
+        exit()
+## memproses spamming
+def sending_messsage(pesan):
+
+    pesan = pesan.replace(" ", "%20") 
+    url = urls + pesan
+
+    try:
+        response = requests.post(url)
+    except requests.exceptions.RequestException as e:
+        print(f"[!] Failed to sent messages: {e}")
+        return
+   
+    print(response.json())
+
+    if response.status_code == 200:
+        print(Fore.GREEN + f"[✓] Successfully sent the message!!. Status code: {response.status_code}\n")
+    else:
+        print(Fore.RED + f"[✗] Failed to sent messages!!. Status code: {response.status_code}\n")
+
+## function yang melakukan looping spamming
+def send_mess(jumReq=250):
+    try:
+        for i in range(jumReq):
+            for message in messages:
+                sendingMesssage(message)
+                time.sleep(dely)
+
+        print(Fore.MAGENTA + "\n[✔ ] Successfully sent all messages")
+        print(Fore.CYAN + "Thanks for using this tool!!")
+    except KeyboardInterrupt:
+        print(Fore.RED + "\nProgram stoped by user")                                                                                                           
+
+#send_mess(message_amount)
+
+if __name__ == "__main__":
+    banner()
+    validate_api()
+    loop_amount()
+    delay()
+    validate_input()
+    get_user_messages()
+    send_mess(message_amount)
